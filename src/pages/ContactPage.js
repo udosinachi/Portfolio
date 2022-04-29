072a1a58574805cd52acb5e49b0fc52572e598ad
Udosinachi <udosipat@gmail.com>
2022-04-29 11:39:29 +0100

    Removed from contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, Form, Button, Badge } from 'react-bootstrap'
+import { Container, Row, Col, Badge } from 'react-bootstrap'
 
 const ContactPage = () => {
   return (
@@ -16,64 +16,6 @@ const ContactPage = () => {
           </Container>
         </Col>
       </Row>
-
-      <Container>
-        <h2 className="name text-center m-auto py-5">CONTACT FORM</h2>
-        <>
-          <Row>
-            <Col md={9} className="m-auto">
-              <Form name="contact-form" method="POST" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact-form" />
-                <Form.Group controlId="formBasicName">
-                  <Form.Control
-                    type="text"
-                    size="lg"
-                    placeholder="Enter Your Name"
-                    name="name"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Control
-                    type="email"
-                    size="lg"
-                    placeholder="Enter Your Email"
-                    name="email"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formBasicSubject">
-                  <Form.Control
-                    type="text"
-                    size="lg"
-                    placeholder="Subject"
-                    name="subject"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Control
-                    as="textarea"
-                    rows={7}
-                    size="lg"
-                    placeholder="Your Message"
-                    name="message"
-                  />
-                </Form.Group>
-                <Button
-                  style={{ fontSize: '16px' }}
-                  variant="dark"
-                  type="submit"
-                  size="lg"
-                  block
-                >
-                  Submit
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </>
-      </Container>
 
       <Container>
         <Row className="py-5">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import Portfolio from '../components/Portfolio'
 
 const HomePage = () => {
@@ -21,6 +21,64 @@ const HomePage = () => {
       <br />
       <br />
       <Portfolio />
+
+      <Container>
+        <h2 className="name text-center m-auto py-5">CONTACT FORM</h2>
+        <>
+          <Row>
+            <Col md={9} className="m-auto">
+              <Form name="contact-form" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact-form" />
+                <Form.Group controlId="formBasicName">
+                  <Form.Control
+                    type="text"
+                    size="lg"
+                    placeholder="Enter Your Name"
+                    name="name"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    size="lg"
+                    placeholder="Enter Your Email"
+                    name="email"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicSubject">
+                  <Form.Control
+                    type="text"
+                    size="lg"
+                    placeholder="Subject"
+                    name="subject"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Control
+                    as="textarea"
+                    rows={7}
+                    size="lg"
+                    placeholder="Your Message"
+                    name="message"
+                  />
+                </Form.Group>
+                <Button
+                  style={{ fontSize: '16px' }}
+                  variant="dark"
+                  type="submit"
+                  size="lg"
+                  block
+                >
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </>
+      </Container>
     </>
   )
 }
