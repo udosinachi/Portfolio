5705320bec6d3f8a5a344f4edf6e964741dc485f
Udosinachi <udosipat@gmail.com>
2022-05-04 13:11:14 +0100

    Update SocialMedia.js

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,44 +3,44 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const SocialMedia = () => {
   return (
-    <div className='bg-light'>
+    <div className="bg-light">
       <Container>
         <Row>
-          <Col className='text-center'>
+          <Col className="text-center">
             <a
-              href='https://github.com/udosinachi'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/udosinachi"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='i fab fa-github my-3 p-3'></i>
+              <i className="i fab fa-github my-3 p-3"></i>
             </a>
             <a
-              href='https://www.linkedin.com/in/udosinachi'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.linkedin.com/in/udosinachi"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='i fab fa-linkedin my-3 p-3'></i>
+              <i className="i fab fa-linkedin my-3 p-3"></i>
             </a>
             <a
-              href='https://www.twitter.com/udosinachii'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.twitter.com/udosinachii"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='i fab fa-twitter my-3 p-3'></i>
+              <i className="i fab fa-twitter my-3 p-3"></i>
             </a>
             <a
-              href='https://wa.me/+2348161993142'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://wa.me/+2348161993142"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='i fab fa-whatsapp my-3 p-3'></i>
+              <i className="i fab fa-whatsapp my-3 p-3"></i>
             </a>
             <a
-              href='https://mail.google.com/mail/u/udosipat@gmail.com'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://mail.google.com/mail/u/devudosinachi@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='i fab fa-google my-3 p-3'></i>
+              <i className="i fab fa-google my-3 p-3"></i>
             </a>
           </Col>
         </Row>
